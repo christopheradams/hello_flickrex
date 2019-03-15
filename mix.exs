2abe9863e18101794d454ee4d7d195d7a9ccf4ee
Christopher Adams <christopher@fabricatorz.com>
2019-03-15 16:25:47 +0800

    Add ueberauth_flickr dep

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule HelloFlickrex.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ueberauth_flickr, "~> 0.2"}
     ]
   end
 end
